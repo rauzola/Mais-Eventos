64699a4b5881c786ebdaefab80470d608edd408a
Raul Sigoli <raul_sigoli@hotmail.com>
2025-09-08 15:45:43 -0300

    feat: add envio de email usando o resend

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,38 +1,26 @@
 // /app/api/email/route.ts
 import { Resend } from 'resend';
+import React from 'react';
+import { EmailTemplate } from '@/components/Email/bem-vindo';
+
+
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
-    const html = `
-      <!doctype html>
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Confirmação de envio</title>
-        </head>
-        <body style="font-family: Arial, sans-serif; color: #111; padding: 16px;">
-          <h1 style="margin: 0 0 12px;">Olá, John!</h1>
-          <p style="margin: 0 0 8px;">Seu e-mail de teste do Projeto Mais Vida foi enviado com sucesso.</p>
-          <p style="margin: 0;">Se você recebeu esta mensagem, o serviço de e-mail está funcionando 👍</p>
-        </body>
-      </html>
-    `;
-
     const { data, error } = await resend.emails.send({
-      from: 'Projeto Mais Vida <no-reply@resend.dev>',
-      to: ['raulsigoli2000@gmail.com'],
-      subject: 'Hello world',
-      html,
+      from: `${process.env.EMAIL_FROM}`,
+      to: ['raul_sigoli@hotmail.com'],
+      subject: 'asdasd world',
+      react: React.createElement(EmailTemplate, { firstName: 'Raul Sigoli' }),
     });
 
     if (error) {
       return Response.json({ error }, { status: 500 });
     }
 
-    return Response.json(data);
+    return Response.json({ data });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
--- src/components/Email/bem-vindo.tsx
+++ src/components/Email/bem-vindo.tsx
@@ -1,15 +1,14 @@
 // /components/Email/bem-vindo.tsx
 import * as React from 'react';
-
 interface EmailTemplateProps {
-  firstName?: string;
+  firstName: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  
+  firstName,
 }) => (
   <div>
-    <h1>Welcome, asdasd!</h1>
+    <h1>Welcome, {firstName}!</h1>
   </div>
 );
 
